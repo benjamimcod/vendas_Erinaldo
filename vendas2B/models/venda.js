14ee2dc9977f9baa5221512f9d9cfc646d8fcc80
NIssapa <nissapa69@gmail.com>
2024-09-20 11:00:48 -0300

    aula20/09/2024
    Salvando arquivos no GitHub

---
+++ vendas2B/models/venda.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Venda extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Venda.belongsTo(models.Cliente,
+        {foreignKey: 'ClienteId',});
+     
+     Venda.belongsTo(models.Vendedor,
+        {foreignKey: 'VendedorId'});
+     
+     Venda.belongsToMany(models.Produto,
+        {foreignKey: 'ProdutoId', });
+     
+     Venda.hasMany(models.Item,
+        {foreignKey: 'VendaId', });
+     
+    }
+  }
+  Venda.init({
+    data: DataTypes.DATE,
+    ClientId: DataTypes.INTEGER,
+    VendedorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Venda',
+  });
+  return Venda;
+};
